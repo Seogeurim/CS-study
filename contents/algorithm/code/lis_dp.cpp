7eb1d5126f6385239a5a9d55bf72898a1d9ce4c9
jjinny <khj9709@icloud.com>
2021-02-18 14:07:33 +0900

    [Algorithm] LIS 누락 코드 및 주석 작성

--- contents/algorithm/code/lis_dp.cpp
+++ contents/algorithm/code/lis_dp.cpp
@@ -23,9 +23,23 @@ int main(){
 			if(dp[i] > top) {
 				top = dp[i];
 				top_i = i;
+				// LIS길이 최댓값을 갖는 인덱스를 top_i에 저장한다.
 			}
 		}
 	}
 
 	cout << top << '\n';
+	
+	// 위에서 저장한 top_i 인덱스부터 시작해서 거꾸로 탐색하면 LIS를 구성하는 요소들을 출력할 수 있다.
+	for(int i=top_i;i>=1;i--) {
+		if(dp[i] == top) {
+			s.push(arr[i]);
+			top--;
+		}
+	}
+
+	while(!s.empty())  {
+		cout << s.top() << ' ';
+		s.pop();
+	}
 }
